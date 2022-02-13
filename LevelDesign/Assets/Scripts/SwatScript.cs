98894a629e04acf54491486f611f02c880680280
JaredMcCu <mccullj3@mail.uc.edu>
2022-02-13 14:14:05 -0500

    movement

---
+++ LevelDesign/Assets/Scripts/SwatScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    Animator anim;
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        float move = Input.GetAxis ("Vertical");
+        anim.SetFloat("Speed", move);
+    }
+}
